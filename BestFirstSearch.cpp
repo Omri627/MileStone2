dea31de55ddecbe5e4cf9ca6ea7222ea1e6c31dd
Omri Sak <omri.sak@live.biu.ac.il>
2019-01-09 18:48:09 +0200

    minor changes

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -6,9 +6,10 @@ Solution* BestFirstSearch<T>::search(Searchable<State<T>> searchable) {
     double newPath, oldPath = 0;
     priorityQueue.emplace(searchable.getInitialState());
     searchable.getInitialState().setColor(GRAY); //is state inside the pq is color is gray
-    //list<State<T>> closed;
+
     while (!priorityQueue.empty()) {
         State<T> n = priorityQueue.pop();
+        n.setColor(BLACK); //color is black so we won't check n again
         if (n == searchable.getGoalState()) {
             double sum = getCostOfPath(searchable.getInitialState(), n);
             double length = getLengthOfPath(searchable.getInitialState(), n);
@@ -16,12 +17,13 @@ Solution* BestFirstSearch<T>::search(Searchable<State<T>> searchable) {
                 return new SearcherSolution(sum, length);
             }
             return new SearcherSolution(-1, -1);
-    }
+        }
+
         list<State<T>> neighbors = searchable.getAllPossibleState(n); //all n neighbors
         for (State<T> s : neighbors) {
             if ((s.getColor() != BLACK) && (s.getColor() != GRAY)) {
                 s->setCameFrom(n);
-                s.setColor(GRAY);
+                s.setColor(GRAY); //color is gray because we add it to queue
                 priorityQueue.emplace(s);
             } else {
                 newPath = getCostOfPath(searchable.getInitialState(), s);
