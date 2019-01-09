d7b55bb0ddda2a15b4b25be464c08809be019db3
Omri Sak <omri.sak@live.biu.ac.il>
2019-01-09 20:37:39 +0200

    priority queue use grater operator

--- MyPriorityQueue.h
+++ MyPriorityQueue.h
@@ -7,6 +7,7 @@ using namespace std;
 template <typename  T>
 class MyPriorityQueue : public priority_queue<T, std::vector<T>, std::greater<T>> {
     //priority queue that use greater instead of less operator
+    //todo:: if the priority queue is not reverse so I need to change the less operator to >
 public:
 
     bool remove(const T& value);
