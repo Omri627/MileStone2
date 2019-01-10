b65b808ca005bfb193d82e0a9367e1c9aa012216
Omri Sak <omri.sak@live.biu.ac.il>
2019-01-10 14:37:59 +0200

    minor changes

--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -4,6 +4,8 @@
 
 #include "Searcher.h"
 #include "MyPriorityQueue.h"
+#include "SearcherSolution.h"
+
 using namespace std;
 
 template <class T>
@@ -12,8 +14,48 @@ private:
     MyPriorityQueue<State<T>> priorityQueue;
 
 public:
-    Solution* search(Searchable<State<T>> searchable);
+    Solution* search(Searchable<State<T>> searchable) {
+        double newPath = 0;
+        double oldPath = -1;
+        priorityQueue.emplace(searchable.getInitialState());
+        searchable.getInitialState().setColor(GRAY); //is state inside the pq is color is gray
+
+        while (!priorityQueue.empty()) {
+            State<T> n = priorityQueue.pop();
+            n.setColor(BLACK); //color is black so we won't check n again
+            if (n == searchable.getGoalState()) {
+                double sum = getCostOfPath(searchable.getInitialState(), n);
+                double length = getLengthOfPath(searchable.getInitialState(), n);
+                if (sum != -1 && length != -1) {
+                    return new SearcherSolution(sum, length);
+                }
+                return new SearcherSolution(-1, -1);
+            }
+
+            list<State<T>> neighbors = searchable.getAllPossibleState(n); //all n neighbors
+            for (State<T> s : neighbors) {
+                if ((s.getColor() != BLACK) && (s.getColor() != GRAY)) {
+                    s->setCameFrom(n);
+                    s.setColor(GRAY); //color is gray because we add it to queue
+                    priorityQueue.emplace(s);
+                } else {
+                    newPath = getCostOfPath(searchable.getInitialState(), s);
+                    if (newPath < oldPath || oldPath == -1) { //old path will be -1 only at the beginning
+                        if (s.getColor() != GRAY) {
+                            //if s is not in pq add to pq
+                            priorityQueue.emplace(s);
+                            oldPath = newPath;
+                        } else {
+                            priorityQueue.remove(s);
+                            priorityQueue.emplace(s);
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
+
 
 
 #endif
